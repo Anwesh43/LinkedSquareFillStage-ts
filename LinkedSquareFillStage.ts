a96c2d5346b7b76a4869838b60d8ae36752d6041
Anwesh43 <anweshthecool0@gmail.com>
2018-05-20 13:22:40 +0530

    adding LSFState class

--- LinkedSquareFillStage.ts
+++ LinkedSquareFillStage.ts
@@ -21,3 +21,29 @@ class LinkedSquareFillStage {
         this.context.fillRect(0, 0, w, h)
     }
 }
+
+class LSFState {
+
+    scale : number = 0
+
+    prevScale : number = 0
+
+    dir : number = 0
+
+    update(stopcb : Function) {
+        this.scale += 0.1 * this.dir
+        if (Math.abs(this.scale - this.prevScale) > 1) {
+            this.scale = this.prevScale + this.dir
+            this.dir = 0
+            this.prevScale = this.scale
+            stopcb()
+        }
+    }
+
+    startUpdating(startcb : Function) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale
+            startcb()
+        }
+    }
+}
